50e3ed1f96482802e9cb14a512e5643627aa57cc
jasonsutter87 <jasonsutter87@gmail.com>
2024-12-02 08:15:06 -0800

    initial commit

---
+++ js/models/models.js
@@ -0,0 +1,314 @@
+const DECK_CARDS = [
+    {
+        "id": 1,
+        "card": "Ace",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 2,
+        "card": "2",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 3,
+        "card": "3",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 4,
+        "card": "4",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 5,
+        "card": "5",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 6,
+        "card": "6",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 7,
+        "card": "7",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 8,
+        "card": "8",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 9,
+        "card": "9",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 10,
+        "card": "10",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 11,
+        "card": "Jack",
+        "suit": "spades",
+        "eyes": 1
+    },
+    {
+        "id": 12,
+        "card": "Queen",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 13,
+        "card": "King",
+        "suit": "spades",
+        "eyes": null
+    },
+    {
+        "id": 14,
+        "card": "Ace",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 15,
+        "card": "2",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 16,
+        "card": "3",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 17,
+        "card": "4",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 18,
+        "card": "5",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 19,
+        "card": "6",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 20,
+        "card": "7",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 21,
+        "card": "8",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 22,
+        "card": "9",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 23,
+        "card": "10",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 24,
+        "card": "Jack",
+        "suit": "hearts",
+        "eyes": 1
+    },
+    {
+        "id": 25,
+        "card": "Queen",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 26,
+        "card": "King",
+        "suit": "hearts",
+        "eyes": null
+    },
+    {
+        "id": 27,
+        "card": "Ace",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 28,
+        "card": "2",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 29,
+        "card": "3",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 30,
+        "card": "4",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 31,
+        "card": "5",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 32,
+        "card": "6",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 33,
+        "card": "7",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 34,
+        "card": "8",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 35,
+        "card": "9",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 36,
+        "card": "10",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 37,
+        "card": "Jack",
+        "suit": "diamonds",
+        "eyes": 2
+    },
+    {
+        "id": 38,
+        "card": "Queen",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 39,
+        "card": "King",
+        "suit": "diamonds",
+        "eyes": null
+    },
+    {
+        "id": 40,
+        "card": "Ace",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 41,
+        "card": "2",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 42,
+        "card": "3",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 43,
+        "card": "4",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 44,
+        "card": "5",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 45,
+        "card": "6",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 46,
+        "card": "7",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 47,
+        "card": "8",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 48,
+        "card": "9",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 49,
+        "card": "10",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 50,
+        "card": "Jack",
+        "suit": "clubs",
+        "eyes": 2
+    },
+    {
+        "id": 51,
+        "card": "Queen",
+        "suit": "clubs",
+        "eyes": null
+    },
+    {
+        "id": 52,
+        "card": "King",
+        "suit": "clubs",
+        "eyes": null
+    }
+]
